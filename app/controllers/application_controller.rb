68385c1c65eb1fc164dfd57cdcc7a62e6f2fc570
B Alter <barnaby.alter@gmail.com>
2013-02-01 17:14:53 -0500

    Fixed caching and reverted to produciton logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -112,8 +112,7 @@ class ApplicationController < ActionController::Base
     elsif !current_user.nil?
       render 'user_sessions/unauthorized_patron'
     else
-      redirect_to :login#, :status => :unauthorized
-      return false
+      render 'user_sessions/logged_out'
     end
   end
 
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,3 +1,7 @@
 class UserSessionsController < ApplicationController
   include Authpds::Controllers::AuthpdsSessionsController
+  
+  def logged_out
+    redirect_to root_url
+  end
 end
