43c4c07fa360a8bdc3f19025d5f52afe8336207a
B Alter <barnaby.alter@gmail.com>
2014-01-31 14:16:49 -0500

    Updated submodules

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Marli::Application.config.session_store :cookie_store, key: '_marli_session', :domain => Settings.cookie.domain
+Marli::Application.config.session_store :cookie_store, key: '_marli_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
