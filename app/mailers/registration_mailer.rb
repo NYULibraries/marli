a4ee481f892e0e133e57fd8215984b198041ff83
Scot Dalton <scotdalton@gmail.com>
2014-10-29 11:14:49 -0400

    Fix name clash in the registration mailer

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,12 +3,12 @@ class RegistrationMailer < ActionMailer::Base
 
   def registration_email(user)
     @user_data = user
-    emails = emails.collect {|email| email["email"]}
+    emails = registration_emails.collect { |email| email["email"] }
     mail(to: emails, subject: t('send_email.subject')) if emails.present?
   end
 
   private
-  def emails
+  def registration_emails
     (Figs.env['REGISTRATION_EMAILS'] || [])
   end
 end
