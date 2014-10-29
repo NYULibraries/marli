a886f5e027c80ccfee71dbfafb41955aa76211ce
Scot Dalton <scotdalton@gmail.com>
2014-10-29 10:30:11 -0400

    Use the Figs environment variables for the registration emails

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,9 +1,14 @@
 class RegistrationMailer < ActionMailer::Base
   default from: "no-reply@library.nyu.edu"
-  
+
   def registration_email(user)
     @user_data = user
-    emails = Settings.emails.collect {|email| email["email"]}
-    mail(:to => emails, :subject => t('send_email.subject'))
+    emails = emails.collect {|email| email["email"]}
+    mail(to: emails, subject: t('send_email.subject')) if emails.present?
+  end
+
+  private
+  def emails
+    (Figs.env['REGISTRATION_EMAILS'] || [])
   end
 end
