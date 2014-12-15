2859555f0c28d34831c806aa562d0e460491bf6b
hab278 <hab278@nyu.edu>
2014-12-15 15:37:02 -0500

    Update spacing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
+
   # Find out if PID file for deactivating app has been set
   def app_is_active?
     (!File.exists? "#{Rails.root}/config/app_is_inactive.pid")
   end
-  
+
   # Generate an abbr tag for long words
   def word_break word
     if word.length > 10
@@ -14,40 +14,40 @@ module ApplicationHelper
       word
     end
   end
-  
+
   # Generate link to sorting action
   def sortable(column, title = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     direction_icon = (direction.eql? "desc") ? :sort_desc : :sort_asc
-    search = params[:search]  
+    search = params[:search]
     html = link_to title, params.merge(:sort => column, :direction => direction, :page => nil, :id => ""), {:class => css_class}
     html << icon_tag(direction_icon) if column == sort_column
     return html
   end
-  
+
   # Get the status of this user
   def user_status(user)
-    return "Admin" if user.user_attributes[:marli_admin]
-    return "Exception" if user.user_attributes[:marli_exception]
+    return "Admin" if user.admin?
+    return "Exception" if user.override_access?
   end
-  
+
   # Find out if use has submitted form already
   def user_submitted(user)
     user.submitted_request
   end
-  
+
   def renewal_checked?
     (!params[:user].nil? && !params[:user][:user_attributes].nil? && !params[:user][:user_attributes][:marli_renewal].nil? && params[:user][:user_attributes][:marli_renewal] == "Renewal")
   end
-  
+
   def user_school
-    
+
   end
-  
+
   def user_department
-    
+
   end
-  
+
 end
