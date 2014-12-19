8356a265fed71edd7e95a7cbca6c0fa8090028a6
hab278 <hab278@nyu.edu>
2014-12-19 17:24:38 -0500

    Create an admin to use for dev purposes

--- test/functional/application_controller_test.rb
+++ test/functional/application_controller_test.rb
@@ -5,7 +5,7 @@ class ApplicationControllerTest < ActionController::TestCase
 
   setup do
     @request.env["devise.mapping"] = Devise.mappings[:admin]
-    sign_in FactoryGirl.create(:admin)
+    sign_in FactoryGirl.create(:admin, username: "admin")
   end
 
   test "should test development functions" do
