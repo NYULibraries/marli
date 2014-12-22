a57774ae4ca5392197bbfb9307dc6eea062ffdd0
B Alter <barnaby.alter@gmail.com>
2014-12-22 13:56:42 -0500

    exlibris-nyu connection error is now a faraday error, so let application controller handle that new exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,22 @@
 require 'json'
 class ApplicationController < ActionController::Base
   layout Proc.new{ |controller| (controller.request.xhr?) ? false : "application" }
-  rescue_from Errno::ECONNREFUSED, :with => :connection_error
-  
+  rescue_from Faraday::ConnectionFailed, :with => :connection_error
+
   include Marli::Affiliations
   helper_method :affiliation_text, :affiliation, :auth_types
   helper_method :detail_by_purpose, :get_sanitized_detail, :text_exists
-    
+
   protect_from_forgery
 
   #Authpds user functions
   include Authpds::Controllers::AuthpdsController
-  
+
   def connection_error
     render 'user_sessions/unexpected_error', :layout => false, :status => 500 and return
   end
   protected :connection_error
-  
+
   # Filter users to root if not admin
   def authenticate_admin
     if !is_admin?
@@ -34,7 +34,7 @@ class ApplicationController < ActionController::Base
   # * If logged in but not authorized, rendered an error page
   # * Otherwise redirect to login page, no anonymous access allowed
   def authorize_patron
-    if is_admin? or is_exception? or is_authorized? 
+    if is_admin? or is_exception? or is_authorized?
       return true
     elsif !current_user.nil?
       render 'user_sessions/unauthorized_patron'
@@ -42,22 +42,22 @@ class ApplicationController < ActionController::Base
       redirect_to login_url unless performed?
     end
   end
-  
+
   # Return true if user is marked as admin
   def is_admin
   	(!current_user.nil? and current_user.user_attributes[:marli_admin])
   end
   alias :is_admin? :is_admin
-  helper_method :is_admin?  
-  
+  helper_method :is_admin?
+
   # Return true if user is marked as an exception
-  # * An 'exception' is a user who doesn't have admin privileges and isn't 
+  # * An 'exception' is a user who doesn't have admin privileges and isn't
   #   an authorized patron status but still is granted access to this app
   def is_exception
     (!current_user.nil? and current_user.user_attributes[:marli_exception])
   end
   alias :is_exception? :is_exception
-  
+
   # Return true if user is an authorized patron status
   def is_authorized
     (!current_user.nil? and auth_types_array.include? current_user.user_attributes[:bor_status])
@@ -69,20 +69,20 @@ class ApplicationController < ActionController::Base
    @current_user ||= User.find_by_username("admin")
   end
   alias :current_user :current_user_dev if Rails.env == "development"
-  
+
   # Protect against SQL injection by forcing column to be an actual column name in the model
   def sort_column klass, default_column = "title_sort"
     klass.constantize.column_names.include?(params[:sort]) ? params[:sort] : default_column
   end
   protected :sort_column
-  
+
   # Protect against SQL injection by forcing direction to be valid
   def sort_direction default_direction = "asc"
     %w[asc desc].include?(params[:direction]) ? params[:direction] : default_direction
   end
   helper_method :sort_direction
   protected :sort_direction
-  
+
   # Return boolean matching the url to find out if we are in the admin view
   def is_in_admin_view
     !request.path.match("/admin").nil?
