0e3fa767bcd995afc47d2f60e0c7f8a24724a988
hab278 <hab278@nyu.edu>
2015-01-14 13:26:34 -0500

    Uncomment out conditional in callback controller that verifies navigational format

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user.save!
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "NYU Libraries") #if is_navigational_format?
+      set_flash_message(:notice, :success, kind: "NYU Libraries") if is_navigational_format?
     else
       session["devise.nyulibraries_data"] = request.env["omniauth.auth"]
       redirect_to root_path
