7a16735601195cd1e229ad9b8e7fba7b264513da
BAlter <barnaby.alter@nyu.edu>
2019-06-28 16:07:57 -0400

    Only load figs when off docker

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,11 @@ require_relative 'boot'
 
 require 'rails/all'
 
-require 'figs'
-# Don't run this initializer on travis.
-Figs.load(stage: Rails.env) unless ENV['CI']
+unless ENV['DOCKER']
+  require 'figs'
+  # Don't run this initializer on travis.
+  Figs.load(stage: Rails.env)
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
